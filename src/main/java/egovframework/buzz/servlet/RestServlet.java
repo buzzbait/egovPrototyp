6b60be7ca231efb68ec53b520b904fc4c7d8cc6a
buzzbait <58794927+buzzbait@users.noreply.github.com>
2020-07-22 14:10:43 +0900

    Update RestServlet.java

--- src/main/java/egovframework/buzz/servlet/RestServlet.java
+++ src/main/java/egovframework/buzz/servlet/RestServlet.java
@@ -11,7 +11,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
+/*
+@RestController 어노테이션은 스프링 4점대 버전부터 지원하는 어노테이션으로, 
+컨트롤러 클래스에 @RestController 만 붙이면 메서드에 @ResponseBody 어노테이션을 붙이지 않아도 문자열과 JSON 등을 전송할 수 있습니다. 
+뷰를 리턴하는 메서드들을 가지고 있는 @Controller와는 다르게 @RestController는 문자열, 객체 등을 리턴하는 메서드들을 가지고 있습니다.
+별도의 설정없이 JSON  변환됨
+*/
 @Configuration
 
 @ComponentScan(	basePackages = "egovframework.buzz",
