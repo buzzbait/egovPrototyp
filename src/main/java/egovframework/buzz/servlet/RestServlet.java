1e38a6e0c8d1cc204e77f1e7fa545a2908c59592
buzzbait <58794927+buzzbait@users.noreply.github.com>
2020-07-23 11:28:11 +0900

    Update RestServlet.java

--- src/main/java/egovframework/buzz/servlet/RestServlet.java
+++ src/main/java/egovframework/buzz/servlet/RestServlet.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 /*
+rest 서블릿은 외부 오픈 api 용으로만 사용 한다.
+(동일was의 웹페이지에서 호출하는 ajax 는 구현 하지 않는다)
 @RestController 어노테이션은 스프링 4점대 버전부터 지원하는 어노테이션으로, 
 컨트롤러 클래스에 @RestController 만 붙이면 메서드에 @ResponseBody 어노테이션을 붙이지 않아도 문자열과 JSON 등을 전송할 수 있습니다. 
 뷰를 리턴하는 메서드들을 가지고 있는 @Controller와는 다르게 @RestController는 문자열, 객체 등을 리턴하는 메서드들을 가지고 있습니다.
